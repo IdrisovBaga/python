f195a0f4fdaa15a8895158e2afe0a3450f413bab
IdrisovBaga <idrisovbaga1@gmail.com>
2024-06-04 21:42:27 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+The_number_of_completed_DZ=12
+The_number_of_hours_spent=1,5
+Course_name= 'Python'
+print(1.5/12)
+Time_For_One_Task=0.125
+print("Python"+', всего задач:'+"12"+',затрачено часов:'+"1,5"+' среднее время выполнения'+" 0.125"+' часа')
