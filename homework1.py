f0ee21d0defb8817079d3957323781e7b1c953c7
IdrisovBaga <idrisovbaga1@gmail.com>
2024-06-04 20:37:50 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+example='Vivobook'#Практическое задание по уроку "Строки и индексация строк"
+print(example[0])
+print(example[-1])
+print(example[5:8])
+print(example[: : -1])
+print(example[1:8:2])
