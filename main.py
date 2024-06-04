0be60a7244162eea4cb1da77ef6004d4b737b8ec
IdrisovBaga <idrisovbaga1@gmail.com>
2024-06-04 19:59:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+a = 'UrbanSuper'#Задача «Длина слова».
+print(len(a))
+first=10 #Задача «Суммы и разности».
+second=6
+print(10+6)
+summa=16
+print(10-6)
+diff=4
+first=10 #Задача «Среднее арифметическое»
+second=8
+third=4
+print((10+8+4)//3)
+mean=7
+first_string=" Вторник" #Задача «Простые строчки»
+second_string='Понедельник'
+print(second_string+first_string)
+a=4 #Задача «Сложная формула».
+b=6
+c=8
+print((a*b)+(a*c))
+f=56
